fe261e0db183fa72a6177e78798a32171032df20
Zhanbolat Orakbayev <fous363@gmail.com>
2024-03-01 20:25:56 +0600

    Change timezone from UTC +06:00 to +05:00

--- src/main/java/com/example/rbapp/constant/DateTimeConstant.java
+++ src/main/java/com/example/rbapp/constant/DateTimeConstant.java
@@ -5,5 +5,5 @@ import java.time.ZoneOffset;
 
 public final class DateTimeConstant {
 
-    public static final ZoneId ALMATY = ZoneId.ofOffset("UTC", ZoneOffset.of("+06:00"));
+    public static final ZoneId ALMATY = ZoneId.ofOffset("UTC", ZoneOffset.of("+05:00"));
 }
